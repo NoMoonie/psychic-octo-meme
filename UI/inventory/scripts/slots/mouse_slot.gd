c36b54d72a3ed5771a02c2c57d5f56ecb0dfca17
NoMoonie <46845957+NoMoonie@users.noreply.github.com>
2021-05-30 22:14:37 +0200

    change folder name and added bg styles

---
+++ UI/inventory/scripts/slots/mouse_slot.gd
@@ -0,0 +1,59 @@
+extends Node2D
+class_name mouse_slot
+
+var slot_index = 6969
+var item = null
+export var in_inventory : bool
+
+onready var itemImg = $container/item
+onready var label = $container/Label
+
+func _ready():
+	refresh()
+
+func pickFromSlot():
+	item = null
+	refresh()
+	
+func putInToSlot(_item):
+	item = _item
+	refresh()
+
+func addRemainder(quantity):
+	var new_item = {
+			item_referance = item.item_referance,
+			quantity = quantity
+		}
+	item = new_item
+	refresh()
+	
+func addItemRemainder(quantity, _item):
+	var new_item = {
+			item_referance = _item.item_referance,
+			quantity = quantity
+		}
+	item = new_item
+	refresh()
+
+func dropItem(in_slot):
+	print(in_slot)
+	if !in_slot:
+		var player = get_tree().get_root().get_node("/root/World/Player")
+		var _item_inst = load("res://Assets/items/item.tscn").instance()
+		_item_inst.global_transform = player.global_transform
+		_item_inst.item = item
+		_item_inst.terrain = "../VoxelTerrain"
+		var root = get_tree().get_root().get_node("/root/World").add_child(_item_inst)
+		pickFromSlot()
+
+func refresh():
+	if item != null:
+		if item.quantity == 1:
+			label.text = ""
+		else:
+			label.text = String(item.quantity)
+		itemImg.texture = item.item_referance.texture
+	else:
+		label.text = ""
+		itemImg.texture = null
+
